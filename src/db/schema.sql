e7a1d836ff20bedda36ea6831cafd7d1f8ef0486
Han Chiang <yaphc@hotmail.com>
2020-05-20 22:30:01 +0800

    Add foreign key and delete cascade to log and chart data table

--- src/db/schema.sql
+++ src/db/schema.sql
@@ -15,8 +15,10 @@ CREATE TABLE `log` (
   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
-  KEY `user_id` (`user_id`)
+  KEY `index_user_id` (`user_id`),
+  CONSTRAINT `fk_user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
 
 CREATE TABLE `chart_data` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -25,5 +27,6 @@ CREATE TABLE `chart_data` (
   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
-  KEY `log_id` (`log_id`)
+  KEY `index_log_id` (`log_id`),
+  CONSTRAINT `fk_log_id` FOREIGN KEY (`log_id`) REFERENCES `log` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
