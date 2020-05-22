12aa668276020cb5c8342c0005b71db46ef75758
Han Chiang <yaphc@hotmail.com>
2020-05-22 10:01:59 +0800

    Fix log table index to replace created_at with datetime

--- src/db/schema.sql
+++ src/db/schema.sql
@@ -16,11 +16,9 @@ CREATE TABLE `log` (
   `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   KEY `index_user_id` (`user_id`),
-  KEY `index_user_id_created_at` (`user_id`,`created_at`),
+  KEY `index_user_id_datetime` (`user_id`,`datetime`),
   CONSTRAINT `fk_user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
-
+) ENGINE=InnoDB AUTO_INCREMENT=52 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci
 
 CREATE TABLE `chart_data` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
